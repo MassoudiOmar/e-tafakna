2f4dce6e2ecbf44536a35048b86c459cfe6baf14
Youssef Bedoui <Jozefbedoui@gmail.com>
2022-05-23 10:14:42 +0100

    finish contractType.questions routes & controller

--- server/controllers/contractType.questions.controller.js
+++ server/controllers/contractType.questions.controller.js
@@ -1,33 +1,38 @@
 const db = require("../database-mysql");
 
-const affectQuestionToContractType =  (req,res)=>{
-let { questions_id, contract_types_id, order_question } = req.body
+const affectQuestionToContractType = (req, res) => {
+    let { questions_id, contract_types_id, order_question } = req.body
 
     const sql = `INSERT into etafakna.questions_has_contract_types (questions_id, contract_types_id, order_question) values (?,?,?)`
-     db.query(sql,[questions_id, contract_types_id, order_question],(err,result)=>{
-         if (err) console.log(err)
-         else res.send(result)
-     })
-   
-}
-
-findQuestionsOfSpecificContract = (req,res)=>{
+    db.query(sql, [questions_id, contract_types_id, order_question], (err, result) => {
+        if (err) console.log(err)
+        else res.send(result)
+    })
 
 }
 
-const findAll =  (req,res)=>{
+const findQuestionsOfSpecificContract = (req, res) => {
+    let contract_id = req.params.contract_id;
+    let query = `SELECT questions_id, order_question FROM etafakna.questions_has_contract_types WHERE contract_types_id  = ?`
+    db.query(query, [contract_id], (err, questions) => {
+        if (err) { console.log(err) }
+        res.json(questions)
+    })
+}
+
+const findAll = (req, res) => {
     try {
         const sql = `SELECT * from questions_has_contract_types`
-         db.query(sql,(err,result)=>{
-             if (err) res.json(err)
-             else res.json(result)
-         }) 
+        db.query(sql, (err, result) => {
+            if (err) res.json(err)
+            else res.json(result)
+        })
 
 
     }
-    catch(err){
+    catch (err) {
         console.log(err)
     }
 }
 
-module.exports = { affectQuestionToContractType, findAll };
+module.exports = { affectQuestionToContractType, findAll , findQuestionsOfSpecificContract };
