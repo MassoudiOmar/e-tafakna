2a9bedbdfd59545eb7e675152c7f03618b7931db
Fares-sghaier# <faroussassg007@gmail.com>
2022-08-18 17:15:42 +0100

    pulling

--- server/controllers/contract.controller.js
+++ server/controllers/contract.controller.js
@@ -2,7 +2,7 @@ const db = require("../database-mysql");
 
 const insertContract = (req, res) => {
   const { contract_types_id } = req.body;
-  const created_at = new moment().format("MMM Do YY"); ;
+  const created_at = new Date() ;
   const sql = `INSERT into contracts (status,contract_url,created_at,contract_types_id) values (?,?,?,?)`;
   db.query(sql, ["draft", "", created_at, contract_types_id], (err, result) => {
     if (err) console.log(err);
