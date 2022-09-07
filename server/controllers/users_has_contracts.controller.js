f2f4cdba804d1e917bef7b26e76891b81638bf6e
amine <amine.selmi@esprit.tn>
2022-09-07 14:25:40 +0100

    last changes

--- server/controllers/users_has_contracts.controller.js
+++ server/controllers/users_has_contracts.controller.js
@@ -28,7 +28,7 @@ formData.append("document", fs.createReadStream("output.docx"));
       {
         headers: formData.getHeaders({
           Authorization:
-            "Bearer pdf_live_45VHZqO3e7bh89x8rAYwjnjMvUC1eBG9xcyJsNSwLcy",
+            "Bearer pdf_live_UIPJ7eyybpwmRv0NpzCMfX4HoGHERNai4U3fHzksiP8",
         }),
         responseType: "stream",
       }
@@ -73,9 +73,17 @@ let getOwner = (req, res) => {
   });
 };
 const sendcontracts = (req, res) => {
+  const date = function today(i) {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth();
+    var yyyy = today.getFullYear();
+    today = dd + "-" + mm + "-" + yyyy;
+    return today;
+  };
   let { owner, contracts_id, receiver } = req.body;
-  const sql = `INSERT INTO users_has_contracts (owner,contracts_id,receiver) VALUES (?,?,?)`;
-  db.query(sql, [owner,contracts_id ,receiver], (err, result) => {
+  const sql = `INSERT INTO users_has_contracts (owner,contracts_id,receiver,date) VALUES (?,?,?,?)`;
+  db.query(sql, [owner, contracts_id, receiver, date()], (err, result) => {
     {
       if (err) console.log(err);
       else res.send(result);
@@ -84,13 +92,29 @@ const sendcontracts = (req, res) => {
 };
 
 const sendNotification = (req, res) => {
-  const date = new Date()
+  const date = function today(i) {
+    var weekday = new Array(7);
+    weekday[0] = "Monday";
+    weekday[1] = "Tuesday";
+    weekday[2] = "Wednesday";
+    weekday[3] = "Thursday";
+    weekday[4] = "Friday";
+    weekday[5] = "Saturday";
+    weekday[6] = "Sunday";
+    var today = new Date();
+    var yyyy = today.getDay();
+    var min = today.getMinutes();
+    var hours = today.getHours();
+    today = weekday[yyyy] + " at " + hours + ":" + min;
+    return today;
+  };
+  const seen = false;
   const { owner, receiver, contracts_id } = req.body;
   console.log(req.body, "bodyyy");
-  const sql = `INSERT INTO users_has_notifications (owner,receiver ,date,contracts_id) VALUES (?,?,?,?)`;
+  const sql = `INSERT INTO users_has_notifications (owner,receiver ,date,contracts_id,seen) VALUES (?,?,?,?,?)`;
   db.query(
     sql,
-    [owner, receiver, date, contracts_id],
+    [owner, receiver, date(), contracts_id, seen],
     (err, result) => {
       if (err) res.send(err);
       else res.send(result);
@@ -105,11 +129,30 @@ const deleteNotification = (req, res) => {
     else res.send(result);
   });
 };
-
+const getnumbers = (req, res) => {
+  let { id } = req.params;
+  const sql = `select * from users_has_notifications where seen != "true" && receiver = ?`;
+  db.query(sql, [id], (err, result) => {
+    if (err) res.send(err);
+    else {
+      res.send(result);
+    }
+  });
+};
+const hasSeen = (req, res) => {
+  const { id } = req.params;
+  const sql = `update users_has_notifications set seen = "true" where id = ?`;
+  db.query(sql, [id], (err, result) => {
+    if (err) res.send(err);
+    else res.send(result);
+  });
+};
 module.exports = {
   userContract,
   getOwner,
   sendcontracts,
   sendNotification,
   deleteNotification,
+  getnumbers,
+  hasSeen,
 };
