dafb09bab3929b5ee6d57e09fbc4c2b40303860b
Fares-sghaier# <faroussassg007@gmail.com>
2022-10-24 15:11:03 +0100

    pushing

--- server/database-mysql/index.js
+++ server/database-mysql/index.js
@@ -3,11 +3,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 var connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "Root",
+  database: process.env.MYSQL_DATABASE || "etafakna",
   charset: "cp1256",
+  port : process.env.DB_PORT || 3306
 });
 
 connection.connect((err, success) => {
