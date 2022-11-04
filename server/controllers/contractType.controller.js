5465710b16c5c88279de230c2eba58b2a75e82db
Fares Sghaier <90458992+Fares-sghaier@users.noreply.github.com>
2022-11-04 17:17:29 +0100

    Update contractType.controller.js

--- server/controllers/contractType.controller.js
+++ server/controllers/contractType.controller.js
@@ -337,7 +337,7 @@ const updateContractImage = async (req, res) => {
         {
           File: docUrl,
         },
-        "xlsx"
+        "docx"
       )
       .then(async function (result) {
         console.log(result.file.url, "doc");
