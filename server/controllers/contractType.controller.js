56307041ee2d7019fbc06064f8607ffed43b526a
Fares Sghaier <90458992+Fares-sghaier@users.noreply.github.com>
2022-11-05 21:12:30 +0100

    Update contractType.controller.js

--- server/controllers/contractType.controller.js
+++ server/controllers/contractType.controller.js
@@ -244,16 +244,17 @@ const updateContractImage = async (req, res) => {
         twoPages == "facture" ? "xlsx" : "docx"
       )
         .then(async function (result) {
-        setTimeout(() => {
+     
+
+        if (i <= Cmpt - 1) urlImage += result.file.url + ",";
+        else urlImage += result.file.url;
+      
           const updateContract = `UPDATE contracts set contract_url = ? , contract_image = ? where id =?`;
           db.query(updateContract, [docUrl, urlImage, id], (err, result) => {
             err ? console.log(err) : console.log(result);
           });
           res.send(urlImage);
-        }, 10000);
-
-        if (i <= Cmpt - 1) urlImage += result.file.url + ",";
-        else urlImage += result.file.url;
+     
       });
   }
 };
