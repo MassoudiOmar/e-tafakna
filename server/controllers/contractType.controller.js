0ce6aacec38d681aa38edab9a25d955d2809135c
Fares Sghaier <90458992+Fares-sghaier@users.noreply.github.com>
2022-12-07 13:31:01 +0100

    Update contractType.controller.js

--- server/controllers/contractType.controller.js
+++ server/controllers/contractType.controller.js
@@ -9,7 +9,7 @@ const FormData = require("form-data");
 const axios = require("axios");
 const Excel = require("exceljs");
 const { type } = require("os");
-var convertapi = require("convertapi")("9hWvgv6JPEObYuRe");
+var convertapi = require("convertapi")("piamGXMsDezzgsRT");
 
 var createDocAndImage = async (str, index, renderObject) => {
   const response = await superagent
