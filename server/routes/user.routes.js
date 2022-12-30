5bb86a682a7e1517d2a64fa84daf8e7068982870
Fares Sghaier <90458992+Fares-sghaier@users.noreply.github.com>
2022-12-30 12:13:28 +0100

    Update user.routes.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -15,6 +15,7 @@ router.put('/updatenot/:id',userController.updateNotifications)
 router.get('/getnotstatus/:id',userController.getnotstatus)
 router.delete('/deleteUser/:userId', userController.deleteUser);
 router.post("/deleteAllNotificationOfUser", userController.deleteAllNotificationOfUser)
+router.post("/getAllAnswerOfUser" , userController.getAllAnswerOfUser)
 // router.post("/addAnswer" ,userController.addAnswer)
 // router.post("/getAllAnswerOfUser" , userController.getAllAnswerOfUser)
 // router.post("/getUserInfoWithId" , userController.getUserInfoWithId)
