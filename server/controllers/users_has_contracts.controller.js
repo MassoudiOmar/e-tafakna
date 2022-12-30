2201165b2570cb0fa60926d72249eecdfb7bf588
Fares Sghaier <90458992+Fares-sghaier@users.noreply.github.com>
2022-12-30 12:37:28 +0100

    Update users_has_contracts.controller.js

--- server/controllers/users_has_contracts.controller.js
+++ server/controllers/users_has_contracts.controller.js
@@ -235,6 +235,15 @@ res.send(result1)
 })
 
 }
+const sentoArchieve = (req, res) => {
+  const id = req.params.id;
+  const sql = `update contracts set archieve = "true" where id = ?` ;
+  db.query(sql, [id], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else res.send(result);
+  });
+};
 
 module.exports = {
   userContract,
@@ -248,6 +257,5 @@ module.exports = {
   getNotification , 
   changeNotification , 
   getContractIdFromPic,
-  sentoArchieve
   
 };
