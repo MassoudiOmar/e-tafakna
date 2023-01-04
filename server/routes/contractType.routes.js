501d80fb37642736af3c7c32fe2fd8cad5dbdd2e
Fares-sghaier# <faroussassg007@gmail.com>
2023-01-04 16:21:15 +0100

    ...

--- server/routes/contractType.routes.js
+++ server/routes/contractType.routes.js
@@ -7,6 +7,7 @@ router.get('/:id',contractType.getDataById)
 router.delete('/delete/:id',contractType.deleteContractById)
 router.post('/fill/:id',contractType.fillContract)
 router.put('/updateImage/:id',contractType.updateContractImage)
+router.post("/concatImages",contractType.concatImages)
 // router.post("/ChangeStatusInContract",contractType.ChangeStatusInContract)
 
 module.exports = router;
