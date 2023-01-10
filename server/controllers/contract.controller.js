8bb0892486dcec52ade989a772b30ff458de26b9
Fares-sghaier# <faroussassg007@gmail.com>
2023-01-10 08:08:31 +0100

    ...

--- server/controllers/contract.controller.js
+++ server/controllers/contract.controller.js
@@ -11,21 +11,7 @@ const insertContract = (req, res) => {
     else res.send(result);
   });
 };
-// const getAllContractByStatus = (req, res) => {
-//   const status = req.params.status;
-//   const owner = req.params.ownerId;
-//   const sql = `SELECT * FROM users_has_contracts c
-//   inner join contracts t on (t.id = c.contracts_id )
-//   inner join contract_types f on (f.id=t.contract_types_id)
-//   inner join users u on(u.id= c.owner)
-//   where t.status = ? && c.owner = ? limit 10,2`;
-//   db.query(sql, [status, owner], (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     } else res.send(result);
-//   });
-// };
-const resultPerPage = 8;
+const resultPerPage = 10;
 const getAllContractByStatus = (req, res, err) => {
   var status = req.params.status;
   var owner = req.params.ownerId;
@@ -38,6 +24,7 @@ const getAllContractByStatus = (req, res, err) => {
     if (err) {
       console.log(err);
     }
+    //Pagination
     const numOfResults = result.length;
     const numberofPAGES0 = Math.ceil(numOfResults / resultPerPage);
     let page = req.query.page ? Number(req.query.page) : 1;
@@ -83,9 +70,12 @@ const getAllContractById = (req, res) => {
   });
 };
 
+///////////////////////////////////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
 let getAllContracts = (req, res) => {
   const { id } = req.params;
-  const sql = `
+  let sql = `
    select c.id, date, uo.username ,uo.image as imageOwner,ur.image as imageReciever, ur.username as receiver,c.created_at,c.contract_url,c.contract_image,ct.signed_time,ct.title_FR,ct.title_AR,ct.title_EN,c.status from users_has_contracts  uhc
       inner join users uo on (uo.id = uhc.owner)
       inner join users ur on (ur.id = uhc.receiver)
@@ -95,12 +85,44 @@ let getAllContracts = (req, res) => {
    `;
   db.query(sql, [id, id], (err, result) => {
     if (err) res.send(err);
-    else {
-      console.log(result, "result");
-      res.send(result);
+
+    //Pagination
+    const numOfResults = result.length;
+    const numberofPAGES0 = Math.ceil(numOfResults / resultPerPage);
+    let page = req.query.page ? Number(req.query.page) : 1;
+    if (page > numberofPAGES0) {
+      console.log("No Data ");
+    } else if (page < 1) {
+      res.send("/?page=" + encodeURIComponent("1"));
     }
+
+    const startingLimit = (page - 1) * resultPerPage;
+    sql = ` 
+    select c.id, date, uo.username ,uo.image as imageOwner,ur.image as imageReciever, ur.username as receiver,c.created_at,c.contract_url,c.contract_image,ct.signed_time,ct.title_FR,ct.title_AR,ct.title_EN,c.status from users_has_contracts  uhc
+       inner join users uo on (uo.id = uhc.owner)
+       inner join users ur on (ur.id = uhc.receiver)
+       inner join contracts c on (c.id = uhc.contracts_id)
+       inner join contract_types ct on (ct.id = c.contract_types_id)
+       WHERE uo.id=? OR ur.id =? LIMIT ${startingLimit},${resultPerPage} 
+    `;
+    db.query(sql, [id, id], (err, result) => {
+      if (err) throw err;
+      let iterator = page - 5 < 1 ? 1 : page - 5;
+      let endingLink =
+        iterator + 9 <= numberofPAGES0
+          ? iterator + 9
+          : page + (numberofPAGES0 + 9);
+      if (endingLink < page + 4) {
+        iterator -= page + 4 - numberofPAGES0;
+      }
+      console.log(endingLink, "endingLink");
+      res.send(result, page, iterator, endingLink, numberofPAGES0);
+    });
   });
 };
+///////////////////////////////////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
 const deleteContract = (req, res) => {
   const imageUri = req.body.imageUri;
   db.query(
