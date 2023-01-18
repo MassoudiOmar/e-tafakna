bb9b805f74e2a2084611059b94ba7fbb7bf3c8dd
Fares-sghaier# <faroussassg007@gmail.com>
2023-01-18 12:44:22 +0100

    ...

--- server/controllers/signatureProcess.js
+++ server/controllers/signatureProcess.js
@@ -185,7 +185,7 @@ const sendOtp = (req, res) => {
   const { certType, userId, idType, authDelivery, phone } = req.body;
   axios
     .post(
-      `https://digigo.tuntrust.tn/tunsign-proxy-webapp/services/rest/tunsign-pro`,
+      `https://digigo.tuntrust.tn/tunsign-proxy-webapp/services/rest/tunsign-proxy-admin/aed-send-otp/`,
       { certType, userId, idType, authDelivery, phone }
     )
     .then((res) => {
