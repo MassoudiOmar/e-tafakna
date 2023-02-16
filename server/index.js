b752ed40246728f62d93345ae2db748a110e72a0
Fares-sghaier# <faroussassg007@gmail.com>
2023-02-16 07:08:30 +0100

    ...

--- server/index.js
+++ server/index.js
@@ -103,23 +103,4 @@ app.use("/uploads", express.static("./uploads"));
 
 
 
-//Confirm the API version from your stripe dashboard
-const stripe = Stripe(SECRET_KEY, { apiVersion: "2020-08-27" });
 
-//"start with nodejs expres?"
-app.post("/create-payment-intent", async (req, res) => {
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: 1099, //lowest denomination of particular currency
-      currency: "usd",
-      payment_method_types: ["card"], //by default
-    });
-    const clientSecret = paymentIntent.client_secret;
-
-    res.json({
-      clientSecret: clientSecret,
-    });
-  } catch (e) {
-    res.json({ error: e.message });
-  }
-});
