8c33623e0e1ce963089d82ea2211ac4a6bc6e149
Fares Sghaier <90458992+Fares-sghaier@users.noreply.github.com>
2023-02-17 11:32:39 +0100

    Update contractType.controller.js

--- server/controllers/contractType.controller.js
+++ server/controllers/contractType.controller.js
@@ -8,7 +8,7 @@ const cloudinary = require("../utils/cloudinary");
 const FormData = require("form-data");
 const axios = require("axios");
 const Excel = require("exceljs");
-var convertapi = require("convertapi")("a462sjrEzllwZHBj");
+var convertapi = require("convertapi")("lmZB7Q2OoMDHogAI");
 const cheerio = require('cheerio');
 const https = require("https");
 /***
