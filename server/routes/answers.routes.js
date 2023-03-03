a191be8ec830cf7d9d10567829cf255f38c79d59
Fares Sghaier <90458992+Fares-sghaier@users.noreply.github.com>
2023-03-03 11:31:43 +0100

    Update answers.routes.js

--- server/routes/answers.routes.js
+++ server/routes/answers.routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const answersController = require("../controllers/answers.controller");
 
 router.post('/AddAnswers',answersController.AddAnswers)
+router.post('/AddAnswer',answersController.AddAnswer)
 router.put('/updateAnswers/:id',answersController.updateAnswers)
 router.get('/get/:contracts_id',answersController.getAnswers)
 router.get('/:id',answersController.getQuestionsAnswers)
